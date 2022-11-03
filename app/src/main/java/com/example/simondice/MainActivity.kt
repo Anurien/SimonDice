6a543f2620e173381443df6add2f593dadd4b629
Nuria <ngarciacobas@danielcastelao.org>
2022-11-03 14:22:10 +0100

    Hecho el metodo que crea la secuencia Secuencia siempre aleatoria

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import kotlinx.coroutines.runBlocking
+import androidx.core.content.res.ResourcesCompat
+import kotlinx.coroutines.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     var secuencia = ArrayList<String>() //Secuncia en ronda actual
     var ronda = 0     //número de ronda
-    var numero = 4    //número de luces encendidas
+    var numero = 1    //número de luces encendidas
     var restante = 0  //número de comprobaciones restantes
+    lateinit var bRed: Button;
+    lateinit var bYellow: Button;
+    lateinit var bBlue: Button;
+    lateinit var bGreen: Button;
+    lateinit var binit: Button;
 
 
     override fun onConfigurationChanged(newConfig: Configuration) {
@@ -30,43 +36,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bRed: Button = findViewById(R.id.red)
-        val bBlue: Button = findViewById(R.id.blue)
-        val bYellow: Button = findViewById(R.id.yellow)
-        val bGreen: Button = findViewById(R.id.green)
-        val binit: Button = findViewById(R.id.start)
+        bRed = findViewById(R.id.red)
+        bBlue = findViewById(R.id.blue)
+        bYellow = findViewById(R.id.yellow)
+        bGreen = findViewById(R.id.green)
+        binit = findViewById(R.id.start)
 
         bRed.setOnClickListener {
-            comprobar("red")
+            comprobar("rojo")
         }
         bBlue.setOnClickListener {
-            comprobar("blue")
+            comprobar("azul")
         }
         bYellow.setOnClickListener {
-            comprobar("Yellow")
+            comprobar("amarillo")
         }
         bGreen.setOnClickListener {
-            comprobar("green")
+            comprobar("verde")
         }
-        binit.setOnClickListener {
-            iniciarPartida()
-        }
+          binit.setOnClickListener {
+              iniciarPartida()
+          }
 
     }
 
-    private fun iniciarPartida() {
-        mostrarSecuencia(numero)
-    }
-
-    private fun generarSecuencia() {
-        val randomInt = (1..4).random()
-        when (randomInt) {
-            1 -> R.drawable.red_button
-            2 -> R.drawable.green_button
-            3 -> R.drawable.blue_button
-            else -> R.drawable.yellow_button
-        }
-    }
+     private fun iniciarPartida() {
+         mostrarSecuencia(numero)
+     }
 
     /**
      * Muesta una secuencia de parpadeos
@@ -76,11 +72,9 @@ class MainActivity : AppCompatActivity() {
 
         var encendido = 0L
 
-        //deshabilitarBotones()
-
         for (i in 1..numero) {
             val random = Random().nextInt(4)
-            encendido += 2500L
+            encendido += 1000L
             when (random) {
                 0 -> {
                     parpadeo(encendido, "azul", numero, i)
@@ -100,63 +94,88 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        numero+1
     }
 
+    /**
+     * Realiza el parpadeo del botón @color
+     * @encendido: Milisegundos de la secuenda a los que se va a encender la luz
+     * @color: Color que se va a encender
+     * @maximo: número de luces totales que se van a encender
+     * @actual: número de luz actual que se va a encender respecto a @maximo de luces que se encienden
+     */
     private fun parpadeo(encendido: Long, color: String, maximo: Int, actual: Int) {
-        when (color) {
-            "azul" -> {
-
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(encendido)
+            when (color) {
+                "azul" -> {
+                    bBlue.setBackgroundResource(R.drawable.blue_on)
+                    delay(500L)
+                    bBlue.setBackgroundResource(R.drawable.blue_button)
+                }
+                "amarillo" -> {
+                    bYellow.setBackgroundResource(R.drawable.yellow_on)
+                    delay(500L)
+                    bYellow.setBackgroundResource(R.drawable.yellow_button)
+                }
+                "verde" -> {
+                    bGreen.setBackgroundResource(R.drawable.green_on)
+                    delay(500L)
+                    bGreen.setBackgroundResource(R.drawable.green_button)
+                }
+                "rojo" -> {
+                    bRed.setBackgroundResource(R.drawable.red_on)
+                    delay(500L)
+                    bRed.setBackgroundResource(R.drawable.red_button)
+                }
             }
-            "amarillo" -> {
-
-            }
-            "verde" -> {
-
-            }
-            "rojo" -> {
+            if (actual >= maximo) {
+                Toast.makeText(this@MainActivity, "Reproduce la secuencia", Toast.LENGTH_SHORT)
+                    .show()
+                restante = numero
 
             }
         }
     }
 
-        /**
-         * Comprueba si el usuario, a la hora de pulsar un botón, ha acertado con la siguiente luz de la secuencia.
-         * Realiza una serie de operaciones según haya acertado, haya fallado o no queden comprobaciones restantes.
-         * @color: Botón que pulsó el usuario
-         */
-        private fun comprobar(color: String) {
-            if (secuencia.isEmpty() == true) {
+    /**
+     * Comprueba si el usuario, a la hora de pulsar un botón, ha acertado con la siguiente luz de la secuencia.
+     * Realiza una serie de operaciones según haya acertado, haya fallado o no queden comprobaciones restantes.
+     * @color: Botón que pulsó el usuario
+     */
+    private fun comprobar(color: String) {
+        if (secuencia.isEmpty() == true) {
+            ronda = 1
+            numero = 3
+            restante = 0
+            Toast.makeText(this@MainActivity, "Presiona Play para jugar", Toast.LENGTH_SHORT)
+                .show()
+            secuencia.clear()
+        } else {
+            if (secuencia.get(0).equals(color)) {
+                secuencia.removeAt(0)
+                Toast.makeText(this@MainActivity, "Acierto", Toast.LENGTH_SHORT).show()
+                restante--
+                if (secuencia.isEmpty() == true) {
+                    numero++
+                    ronda++
+                    runBlocking {
+                        Toast.makeText(this@MainActivity, "Ronda: $ronda", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    mostrarSecuencia(numero)
+                }
+            } else {
                 ronda = 1
                 numero = 3
                 restante = 0
-                Toast.makeText(this@MainActivity, "Presiona Play para jugar", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(
+                    this@MainActivity,
+                    "Ohhh...has fallado,vuelve a intentarlo",
+                    Toast.LENGTH_SHORT
+                ).show()
                 secuencia.clear()
-            } else {
-                if (secuencia.get(0).equals(color)) {
-                    secuencia.removeAt(0)
-                    Toast.makeText(this@MainActivity, "Acierto", Toast.LENGTH_SHORT).show()
-                    restante--
-                    if (secuencia.isEmpty() == true) {
-                        numero++
-                        ronda++
-                        runBlocking {
-                            Toast.makeText(this@MainActivity, "Ronda: $ronda", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                        mostrarSecuencia(numero)
-                    }
-                } else {
-                    ronda = 1
-                    numero = 3
-                    restante = 0
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Ohhh...has fallado,vuelve a intentarlo",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    secuencia.clear()
-                }
             }
         }
     }
+}
